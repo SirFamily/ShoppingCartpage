fc7f673c8b001800571a4abc570cb5fbb3a86dfb
thiraphat.ch <thiraphat.ch@rmuti.ac.th>
2023-12-12 02:54:04 +0700

    Update UI and add functionality to the shopping cart. (generated by blackbox.ai )

---
+++ javascript.js
@@ -0,0 +1,89 @@
+function changeDotColorDocs() {
+    var selectedValue = document.getElementById("docs").value;
+
+    if (selectedValue === "0") {
+        document.getElementById("dot-d-js1").style.backgroundColor = "rgb(160, 160, 160)";
+        document.getElementById("dot-d-js2").style.backgroundColor = "rgb(160, 160, 160)";
+        document.getElementById("dot-d-js3").style.backgroundColor = "rgb(160, 160, 160)";
+        document.getElementById("price-docs").innerHTML = "0";
+        document.getElementById('docs-js').innerHTML= "";
+    } else if (selectedValue === "1") {
+        document.getElementById("dot-d-js1").style.backgroundColor = "#00DBB6";
+        document.getElementById("dot-d-js2").style.backgroundColor = "rgb(160, 160, 160)";
+        document.getElementById("dot-d-js3").style.backgroundColor = "rgb(160, 160, 160)";
+        document.getElementById("price-docs").innerHTML = "75";
+        document.getElementById('docs-js').innerHTML= "Google Slides LV1";
+    } else if (selectedValue === "2") {
+        document.getElementById("dot-d-js1").style.backgroundColor = "#00DBB6";
+        document.getElementById("dot-d-js2").style.backgroundColor = "#00DBB6";
+        document.getElementById("dot-d-js3").style.backgroundColor = "rgb(160, 160, 160)";
+        document.getElementById("price-docs").innerHTML = "150";
+        document.getElementById('docs-js').innerHTML= "Google Slides LV2";
+    } else if (selectedValue === "3") {
+        document.getElementById("dot-d-js1").style.backgroundColor = "#00DBB6";
+        document.getElementById("dot-d-js2").style.backgroundColor = "#00DBB6";
+        document.getElementById("dot-d-js3").style.backgroundColor = "#00DBB6";
+        document.getElementById("price-docs").innerHTML = "225";
+        document.getElementById('docs-js').innerHTML= "Google Slides LV3";
+
+    }
+    priceDoc = parseInt(document.getElementById('price-docs').innerHTML);
+  updateTotal();
+}
+
+function changeDotColorSlides() {
+    var selectedValue = document.getElementById("slides").value;
+
+    if (selectedValue === "0") {
+        document.getElementById("dot-s-js1").style.backgroundColor = "rgb(160, 160, 160)";
+        document.getElementById("dot-s-js2").style.backgroundColor = "rgb(160, 160, 160)";
+        document.getElementById("dot-s-js3").style.backgroundColor = "rgb(160, 160, 160)";
+        document.getElementById("dot-s-js4").style.backgroundColor = "rgb(160, 160, 160)";
+        document.getElementById("price-slides").innerHTML = "0";
+         document.getElementById("slides-js").innerHTML = "";
+    } else if (selectedValue === "1") {
+        document.getElementById("dot-s-js1").style.backgroundColor = "#00DBB6";
+        document.getElementById("dot-s-js2").style.backgroundColor = "rgb(160, 160, 160)";
+        document.getElementById("dot-s-js3").style.backgroundColor = "rgb(160, 160, 160)";
+        document.getElementById("dot-s-js4").style.backgroundColor = "rgb(160, 160, 160)";
+        document.getElementById("price-slides").innerHTML = "120";
+        document.getElementById("slides-js").innerHTML = "Google Slides LV1";
+    } else if (selectedValue === "2") {
+        document.getElementById("dot-s-js1").style.backgroundColor = "#00DBB6";
+        document.getElementById("dot-s-js2").style.backgroundColor = "#00DBB6";
+        document.getElementById("dot-s-js3").style.backgroundColor = "rgb(160, 160, 160)";
+        document.getElementById("dot-s-js4").style.backgroundColor = "rgb(160, 160, 160)";
+        document.getElementById("price-slides").innerHTML = "240";
+        document.getElementById("slides-js").innerHTML = "Google Slides LV2";
+    } else if (selectedValue === "3") {
+        document.getElementById("dot-s-js1").style.backgroundColor = "#00DBB6";
+        document.getElementById("dot-s-js2").style.backgroundColor = "#00DBB6";
+        document.getElementById("dot-s-js3").style.backgroundColor = "#00DBB6";
+        document.getElementById("dot-s-js4").style.backgroundColor = "rgb(160, 160, 160)";
+        document.getElementById("price-slides").innerHTML = "360";
+        document.getElementById("slides-js").innerHTML = "Google Slides LV3";
+    } else if (selectedValue === "4") {
+        document.getElementById("dot-s-js1").style.backgroundColor = "#00DBB6";
+        document.getElementById("dot-s-js2").style.backgroundColor = "#00DBB6";
+        document.getElementById("dot-s-js3").style.backgroundColor = "#00DBB6";
+        document.getElementById("dot-s-js4").style.backgroundColor = "#00DBB6";
+        document.getElementById("price-slides").innerHTML = "480";
+        document.getElementById("slides-js").innerHTML = "Google Slides LV4";
+    }
+    priceSlid = parseInt(document.getElementById('price-slides').innerHTML);
+  updateTotal();
+}
+
+
+var priceDoc = parseInt(document.getElementById('price-docs').innerHTML);
+var priceSlid = parseInt(document.getElementById('price-slides').innerHTML);
+
+function updateTotal() {
+  var total = priceDoc + priceSlid;
+  var tax  = total * 0.07;
+  document.getElementById('to-price').innerHTML = "THB" + tax.toFixed(2) + " + tax 7%";
+  document.getElementById('price').innerHTML = "THB " + total;
+}
+
+updateTotal();
+
